162af8c21a9b7bd1e365a784aa4f333b83fac422
yijie97 <yng7@sva.edu>
2021-09-23 12:02:46 -0400

    thought experiment

---
+++ experiment.py
@@ -0,0 +1,22 @@
+# this is athought experiment
+
+# this is an array
+Floors = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" ]
+
+# loop through items in array
+for Floor in Floors:
+    print(Floor)
+
+# if im on 1st floor, the elevator needs to go up
+#if (Floors < 1):
+       # print "Going Up"
+
+# if im on 2nd - 10th floor, the elevator needs to go down
+#if (Floors > 1):
+        #print "Going down"
+
+# I'm unsure of what I'm doing....
+
+
+
+
